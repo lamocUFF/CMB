11801c73cb80de311b0c8048894a7ec9d54d6f2c
Laboratório de modelagem Climática <lamoc.git@gmail.com>
2016-04-04 15:05:06 -0300

    xx

--- adquire.sh
+++ adquire.sh
@@ -28,6 +28,44 @@
 export LANG=en_us_8859_1
 
 
+export PATH=$PATH:/usr/local/grads
+export GADDIR=/usr/local/grads
+export GADLIB=/usr/local/grads
+export GASCRP=/usr/local/grads
+
+
+MACH=`uname -a | cut -c1-5` 
+if [ $MACH = "Linux" ];then 
+export PATH=$PATH:/usr/local/grads
+export GADDIR=/usr/local/grads
+export GADLIB=/usr/local/grads
+export GASCRP=/usr/local/grads
+fi 
+
+ #%
+#% maquina da UFF 
+#%
+MACH=`uname -a | cut -c7-11` 
+if [ $MACH = "DEAMA" ];then 
+export PATH=/home/cataldi/SCRIPT/grads:$PATH
+export GADDIR=/home/cataldi/SCRIPT/grads
+export GADLIB=/home/cataldi/SCRIPT/rads
+export GASCRP=/home/cataldi/SCRIPT/grads
+fi 
+
+
+MACH=`hostname` 
+if [ $MACH = "clsrv1" ];then 
+echo "             hostname clsrv1 "
+cd /home/anderson/Downloads/CMB-master
+export PATH=$PATH:/usr/local/grads
+export GADDIR=/usr/local/grads
+export GADLIB=/usr/local/grads
+export GASCRP=/usr/local/grads
+
+fi 
+
+
 ./adquire_eta40.sh $1
 ./adquire_obs.sh $1
 ./adquire_gfs.sh  $1
